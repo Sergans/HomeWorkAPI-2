4aba6173f40fa7a1ed6e607f25b45fc39b4f703f
Сергей <gan7er@yandex.ru>
2021-06-27 10:40:57 +0300

    add sqltest to AgentMetrics

---
+++ TaskAPI-2-1/MetricsAgent/CpuMetric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent
+{
+    public class CpuMetric
+    {
+        public int Id { get; set; }
+
+        public int Value { get; set; }
+
+        public long Time { get; set; }
+
+    }
+}
