362fa9c4bb668f5262130f268098dfe2ff2be518
Сергей <gan7er@yandex.ru>
2021-07-19 17:47:17 +0300

    add table agents

---
+++ TaskAPI-2-1/TaskAPI-2-1/DAL/Migrations/AgentMigration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace TaskAPI_2_1.DAL.Migrations
+{
+    [Migration(6)]
+    public class AgentMigration:Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("agents");
+        }
+
+        public override void Up()
+        {
+            Create.Table("agents")
+                 .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                 .WithColumn("agentId").AsInt32()
+                 .WithColumn("AgentUrl").AsString();
+                 
+        }
+
+    }
+}
