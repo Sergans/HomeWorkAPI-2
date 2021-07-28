1a42b482eeb64e417ed44ce5ce077831ef10ac87
Сергей <gan7er@yandex.ru>
2021-07-28 21:08:08 +0300

    change program.cs

--- TaskAPI-2-1/MetricsAgent/Program.cs
+++ TaskAPI-2-1/MetricsAgent/Program.cs
@@ -40,6 +40,7 @@ namespace MetricsAgent
             Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
+                webBuilder.UseUrls("http://localhost:5010");
             })
             .ConfigureLogging(logging =>
             {
