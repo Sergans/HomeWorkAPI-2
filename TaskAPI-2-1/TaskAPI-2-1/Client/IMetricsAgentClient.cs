c4332611aa2947883abdcc92bc280d1d9f9abacd
Сергей <gan7er@yandex.ru>
2021-08-02 19:54:00 +0300

    Нужно поменять формат строки

--- TaskAPI-2-1/TaskAPI-2-1/Client/IMetricsAgentClient.cs
+++ TaskAPI-2-1/TaskAPI-2-1/Client/IMetricsAgentClient.cs
@@ -32,8 +32,8 @@ namespace TaskAPI_2_1.Client
         {
             //var fromParameter = request.FromTime.ToUnixTimeSeconds();
             //var toParameter = request.ToTime.ToUnixTimeSeconds();
-            var fromParameter = request.FromTime.UtcDateTime;
-            var toParameter = request.ToTime.UtcDateTime;
+            var fromParameter = request.FromTime;
+            var toParameter = request.ToTime;
 
            var httpRequest = new HttpRequestMessage(HttpMethod.Get,$"{request.ClientBaseAddress}/{fromParameter}/to/{toParameter}");
            // var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"{request.ClientBaseAddress= "http://localhost:5010/api/metrics/cpu/from/2021-07-30Z14:00:30/to/2021-07-30Z14:00:45"}");
