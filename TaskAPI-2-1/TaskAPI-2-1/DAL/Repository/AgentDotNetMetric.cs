56155149cc29a670239230e7b11f711122e67e1a
Сергей <gan7er@yandex.ru>
2021-08-08 15:05:56 +0300

    add repository DotNet

--- TaskAPI-2-1/TaskAPI-2-1/DAL/Repository/AgentDotNetMetric.cs
+++ TaskAPI-2-1/TaskAPI-2-1/DAL/Repository/AgentDotNetMetric.cs
@@ -3,34 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TaskAPI_2_1.Agents.Model;
+using Dapper;
+using System.Data.SQLite;
+using TaskAPI_2_1.IConectionManager;
+
 
 namespace TaskAPI_2_1.DAL.Repository
 {
     public class AgentDotNetMetric : IAgentDotNetMetric
     {
+        public AgentDotNetMetric()
+        {
+            SqlMapper.AddTypeHandler(new DateTimeOffsetHandler());
+        }
+        IConectionOpen connectionstring = new ConectionOpen();
         public void Create(DotNetAgent item, int id)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            connection.Execute("INSERT INTO dotnetagentmetrics(agentId,value,time) VALUES(@agentId,@value, @time)",
+                new
+                {
+                    value = item.Value,
+                    time = item.Time.ToUnixTimeSeconds(),
+                    agentId = id
+                });
         }
 
         public List<AgentInfo> GetAgentAdress()
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            return connection.Query<AgentInfo>("SELECT * FROM agents").ToList();
         }
 
         public IList<DotNetAgent> GetAgentMetricPeriod(int agentId, DateTimeOffset fromTime, DateTimeOffset toTime)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            return connection.Query<DotNetAgent>("SELECT id,agentId,value,time FROM dotnetagentmetrics WHERE agentId=@agentId AND time>@fromTime AND time<@toTime", new
+            {
+                fromTime = fromTime.ToUnixTimeSeconds(),
+                toTime = toTime.ToUnixTimeSeconds(),
+                agentId = agentId.ToString()
+            }).ToList();
         }
 
         public IList<DotNetAgent> GetAllMetricPeriod(DateTimeOffset fromTime, DateTimeOffset toTime)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            return connection.Query<DotNetAgent>("SELECT id,agentId,value,time FROM dotnetagentmetrics WHERE time>@fromTime AND time<@toTime", new
+            {
+                fromTime = fromTime.ToUnixTimeSeconds(),
+                toTime = toTime.ToUnixTimeSeconds(),
+
+            }).ToList();
         }
 
         public DateTimeOffset GetMaxDateTime(int id)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+
+            return connection.Query<DateTimeOffset>("SELECT time FROM dotnetagentmetrics WHERE agentId=@id", new { id = id }).Max();
         }
     }
 }
