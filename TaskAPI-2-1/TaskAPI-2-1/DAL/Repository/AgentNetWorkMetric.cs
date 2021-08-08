4640498d9f317242d9427b0357a573fc5989d345
Сергей <gan7er@yandex.ru>
2021-08-08 15:17:00 +0300

    add metods all repository

--- TaskAPI-2-1/TaskAPI-2-1/DAL/Repository/AgentNetWorkMetric.cs
+++ TaskAPI-2-1/TaskAPI-2-1/DAL/Repository/AgentNetWorkMetric.cs
@@ -11,29 +11,56 @@ namespace TaskAPI_2_1.DAL.Repository
 {
     public class AgentNetWorkMetric : IAgentNetWorkMetric
     {
+        public AgentNetWorkMetric()
+        {
+            SqlMapper.AddTypeHandler(new DateTimeOffsetHandler());
+        }
+        IConectionOpen connectionstring = new ConectionOpen();
         public void Create(NetWorkAgent item, int id)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            connection.Execute("INSERT INTO networkagentmetrics(agentId,value,time) VALUES(@agentId,@value, @time)",
+                new
+                {
+                    value = item.Value,
+                    time = item.Time.ToUnixTimeSeconds(),
+                    agentId = id
+                });
         }
 
         public List<AgentInfo> GetAgentAdress()
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            return connection.Query<AgentInfo>("SELECT * FROM agents").ToList();
         }
 
         public IList<NetWorkAgent> GetAgentMetricPeriod(int agentId, DateTimeOffset fromTime, DateTimeOffset toTime)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            return connection.Query<NetWorkAgent>("SELECT id,agentId,value,time FROM networkagentmetrics WHERE agentId=@agentId AND time>@fromTime AND time<@toTime", new
+            {
+                fromTime = fromTime.ToUnixTimeSeconds(),
+                toTime = toTime.ToUnixTimeSeconds(),
+                agentId = agentId.ToString()
+            }).ToList();
         }
 
         public IList<NetWorkAgent> GetAllMetricPeriod(DateTimeOffset fromTime, DateTimeOffset toTime)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            return connection.Query<NetWorkAgent>("SELECT id,agentId,value,time FROM networkagentmetrics WHERE time>@fromTime AND time<@toTime", new
+            {
+                fromTime = fromTime.ToUnixTimeSeconds(),
+                toTime = toTime.ToUnixTimeSeconds(),
+
+            }).ToList();
         }
 
         public DateTimeOffset GetMaxDateTime(int id)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+
+            return connection.Query<DateTimeOffset>("SELECT time FROM networkagentmetrics WHERE agentId=@id", new { id = id }).Max();
         }
     }
 }
--- TaskAPI-2-1/TaskAPI-2-1/DAL/Repository/AgentRamMetric.cs
+++ TaskAPI-2-1/TaskAPI-2-1/DAL/Repository/AgentRamMetric.cs
@@ -11,29 +11,56 @@ namespace TaskAPI_2_1.DAL.Repository
 {
     public class AgentRamMetric : IAgentRamMetric
     {
+        public AgentRamMetric()
+        {
+            SqlMapper.AddTypeHandler(new DateTimeOffsetHandler());
+        }
+        IConectionOpen connectionstring = new ConectionOpen();
         public void Create(RamAgent item, int id)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            connection.Execute("INSERT INTO ramagentmetrics(agentId,value,time) VALUES(@agentId,@value, @time)",
+                new
+                {
+                    value = item.Value,
+                    time = item.Time.ToUnixTimeSeconds(),
+                    agentId = id
+                });
         }
 
         public List<AgentInfo> GetAgentAdress()
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            return connection.Query<AgentInfo>("SELECT * FROM agents").ToList();
         }
 
         public IList<RamAgent> GetAgentMetricPeriod(int agentId, DateTimeOffset fromTime, DateTimeOffset toTime)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            return connection.Query<RamAgent>("SELECT id,agentId,value,time FROM ramagentmetrics WHERE agentId=@agentId AND time>@fromTime AND time<@toTime", new
+            {
+                fromTime = fromTime.ToUnixTimeSeconds(),
+                toTime = toTime.ToUnixTimeSeconds(),
+                agentId = agentId.ToString()
+            }).ToList();
         }
 
         public IList<RamAgent> GetAllMetricPeriod(DateTimeOffset fromTime, DateTimeOffset toTime)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+            return connection.Query<RamAgent>("SELECT id,agentId,value,time FROM ramagentmetrics WHERE time>@fromTime AND time<@toTime", new
+            {
+                fromTime = fromTime.ToUnixTimeSeconds(),
+                toTime = toTime.ToUnixTimeSeconds(),
+
+            }).ToList();
         }
 
         public DateTimeOffset GetMaxDateTime(int id)
         {
-            throw new NotImplementedException();
+            using var connection = new SQLiteConnection(connectionstring.GetOpenedConection());
+
+            return connection.Query<DateTimeOffset>("SELECT time FROM ramagentmetrics WHERE agentId=@id", new { id = id }).Max();
         }
     }
 }
